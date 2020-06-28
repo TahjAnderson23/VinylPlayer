d7164c36521adca993a73966233b395d5bb4d9cb
Tahj Anderson <tahjanderson@Tahjs-MacBook-Pro.local>
2020-06-28 18:25:30 -0400

    Second push

---
+++ src/config.js
@@ -0,0 +1,9 @@
+export const authEndpoint = "https://accounts.spotify.com/authorize";
+export const clientId = "9c2e39c0301b4002ab95e58bcb3fc9c4";
+export const redirectUri = 'http://localhost:3000';
+
+export const scopes = [
+    "user-top-read",
+    "user-read-currently-playing",
+    "user-read-playback-state",
+];
